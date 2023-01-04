b55c057e656388443ad5e28886135505b27beba6
Alex <joubert2006@hotmail.com>
2023-01-04 02:41:28 -0300

    Criação das rotas

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { ThemeProvider } from '@mui/material'
+import { Routes, Route } from 'react-router-dom'
+import AuthContext from './Components/Context/AuthContext'
 import { Home } from './Pages/Home'
+import { Login } from './Pages/Login'
 import { Pokemon } from './Pages/Pokemon'
 
 export default function MainRoutes() {
+  const { theme } = useContext(AuthContext)
   return (
-    <BrowserRouter>
+    <ThemeProvider theme={theme}>
       <Routes>
         <Route element={<Home />} path="/" />
-        <Route element={<Pokemon />} path="/pokemon" />
+        <Route element={<Pokemon />} path="/pokemon/:pokemonIndex" />
+        <Route element={<Login />} path="/login" />
       </Routes>
-    </BrowserRouter>
+    </ThemeProvider>
   )
 }
